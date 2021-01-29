464a06912912e8eadc9704d01d8aa93adb4c8562
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-01-29 06:58:24 -0500

    custom user alias definitions
    Putting them in this repository for safe-keeping and ease of access on new unix-like systems

---
+++ alias_definitions.sh
@@ -0,0 +1,28 @@
+# -------------------------------------------------------------------------------------------------------------------------
+
+# CUSTOM USER ALIAS DEFINITIONS                                                               
+                                                                                              
+                                                                                              
+# Terminal ease-of-use aliases                                                                
+alias cls='clear && ls' # Great command. Clears terminal and lists files in current dir       
+alias clls='clear && ls -l'                                                                   
+alias clas='clear && ls -a'                                                                   
+alias cllas='clear && ls -la'                                                                 
+alias tailhist='history | tail -n $1'                                                         
+alias histgrep='history | grep -i $1'                                                         
+alias authorize='sudo chmod +x'                                                               
+                                                                                              
+                                                                                              
+# emacs alias definitions                                                                                                                                           
+alias anthomacs='emacs -nw -bg color-20 -fg yellow' 
+
+
+
+
+
+
+# -------------------------------------------------------------------------------------------------------------------------
+
+
+
+
