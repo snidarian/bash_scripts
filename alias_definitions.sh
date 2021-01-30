d2812ea5c07e4d2c663c280756982d9a26801243
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-01-30 08:27:14 -0500

    Update alias_definitions.sh

--- alias_definitions.sh
+++ alias_definitions.sh
@@ -8,10 +8,15 @@ alias cls='clear && ls' # Great command. Clears terminal and lists files in curr
 alias clls='clear && ls -l'                                                                   
 alias clas='clear && ls -a'                                                                   
 alias cllas='clear && ls -la'                                                                 
+
+# history search aliases
 alias tailhist='history | tail -n $1'                                                         
-alias histgrep='history | grep -i $1'                                                         
-alias authorize='sudo chmod +x'                                                               
-                                                                                              
+alias histgrep='history | grep -i $1'
+alias histcount='history | grep -ic $1'
+
+# user permissions aliases
+alias authorize='sudo chmod +766'                                                               
+alias deauthorize='sudo chmod -777'                                                                                              
                                                                                               
 # emacs alias definitions                                                                                                                                           
 alias anthomacs='emacs -nw -bg color-20 -fg yellow' 
