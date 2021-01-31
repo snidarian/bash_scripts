33b5df2daa9b1a0aa4e1361ab8e25d6d04e72f3c
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-01-31 06:56:46 -0500

    Update alias_definitions.sh

--- alias_definitions.sh
+++ alias_definitions.sh
@@ -5,9 +5,9 @@
                                                                                               
 # Terminal ease-of-use aliases                                                                
 alias cls='clear && ls' # Great command. Clears terminal and lists files in current dir       
-alias clls='clear && ls -l'                                                                   
-alias clas='clear && ls -a'                                                                   
-alias cllas='clear && ls -la'                                                                 
+alias clls='clear && ls -l'  # clears and lists with long list flag specifier                         
+alias clas='clear && ls -a'  # clears and list with hidden file flag specifier                                                                 
+alias cllas='clear && ls -la' # Same but with long and hidden file flags                
 alias numberedls='j=0; for i in `ls`; do j=$(expr $j + 1); echo "$j. $i"; done' # numbers ls output. Can be useful. Has bug: doesn't handle whitespaces well but that's more a fault of the bash for loop than anything. I'll figure out how to fix it
 
 # history search aliases
