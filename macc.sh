4f649ccacd7af1e9c770681e4557e46dc22f3389
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-02-15 15:51:45 -0500

    Update macc.sh

--- macc.sh
+++ macc.sh
@@ -1,18 +1,33 @@
-#! /bin/bash
-# must have macchanger program installed to run this script
-# double check the names of your network devices and change them from wlan0, and eth0 as necessary
+#! /bin/bash                                                                                  
 
-echo "reconfiguring wifi and ethernet..."
+# macchanger program and ifconfig command are required to use this script.
+# Rename $wifidev and $ethdev to your specific hardware device-names
 
-echo "wlan0"
-sudo ifconfig wlan0 down && sudo macchanger -r wlan0 && sudo ifconfig wlan0 up
-echo "eth0"
-sudo ifconfig eth0 down && sudo macchanger -r eth0 && sudo ifconfig eth0 up
+# must have macchanger program installed to run this script                                   
+# double check the names of your network devices and change them from wlan0, and eth0 as nece\
+ssary                                                                                         
+
+# NOTE                                                                                        
+# if your system does not possess the ifconfig command,                                       
+# You can download it in the debian package names "net-tools"                                 
+
+# Set these variables equal to your device names                                              
+wifidev="wlp8s0"
+ethdev="enp4s0"
+
+echo "################MACADDRESSREASSIGN#################"
+
+echo "reconfiguring wifi and ethernet mac addresses..."
+
+echo "$wifidev"
+sudo ifconfig $wifidev down && sudo macchanger -r $wifidev && sudo ifconfig $wifidev up
+echo "$ethdev"
+sudo ifconfig $ethdev down && sudo macchanger -r $ethdev && sudo ifconfig $ethdev up
 echo "------------------------------------------------"
 echo "eth0 and wlan0 mac addresses randomly configured"
 echo "------------------------------------------------"
 
 
-
+echo "################MACADDRESSREASSIGN#################"
 
 
