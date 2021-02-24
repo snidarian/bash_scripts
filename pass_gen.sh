37d4dfb2889eb5184253bcf7faf157f30be7f607
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-02-24 15:51:38 -0500

    Update pass_gen.sh

--- pass_gen.sh
+++ pass_gen.sh
@@ -13,21 +13,29 @@ reset="\e[0m"
 dashband="${ybold}======================================================================${reset}"
 
 
-passquantity=${1:-10}
-passlength=${2:-10}
-outputfilename=${3:-"pass.txt"}
+passquantity=${1:-64}
+passlength=${2:-64}
+generationalgorithm=${3:-0}
+outputfilename=${4:-"pass.txt"}
+
+
 
 
 # help message output on request
 if [ $passquantity == "--help" ]; then
-    echo -e "======================================================\n"
-    echo -en "${redbold}ARGV1${reset} - Password quantity\n\n${redbold}ARGV2${reset} - Password lengh\n\n${redbold}ARGV3${reset} - Outputfile (${yellow}pass.txt by default${reset})\n\n"
-    echo "=====================================================\n"
+    echo -e "${ybold}======================================================${reset}\n"
+    echo -en "${redbold}ARGV1${reset} - ${ybold}Quantity${reset} (default=64)\n\n${redbold}ARGV2${reset} - ${ybold}Password length${reset} (default=64)\n\n${redbold}ARGV3${reset} - ${ybold}Algorithm${reset} (default=0)\n\n${redbold}ARGV4${reset} - ${ybold}Outputfile name${reset} (default=pass.txt)\n\n"
+    echo -e "${ybold}=====================================================${reset}\n"
     exit 0
 
 # else output arg values if supplied and output defaults if not
     
 else
+
+    if [ $generationalgorithm -gt 1 ]; then
+	echo -e "${redbold}Error: Select 0 (default) or 1 as ARGV3${reset}"
+	exit -1
+    fi
     
     if [ $passlength -gt 255 ]; then
 	echo -e "${redbold}Error: Max length is 255${reset}"
@@ -35,12 +43,12 @@ else
     fi
     echo -e "${dashband}" >> $outputfilename
     passwordbatchid=$(apg -n 1 -x 5 -m 5)
-    echo -ne "Password Batch ID: ${redbold}$passwordbatchid${reset}\n\n" >> $outputfilename
-    echo -ne "${ybold}$passquantity${reset} passwords at ${ybold}$passlength${reset} chars-length:\n\n" >> $outputfilename
+    echo -ne "Password Batch ID: ${redbold}$passwordbatchid ${reset}\t${ybold}Generated - $(date +%D) at ($(date +%H:%M))${reset}\n\n" >> $outputfilename
+    echo -ne "${ybold}$passquantity${reset} passwords at ${ybold}$passlength${reset} char-length:\n\n" >> $outputfilename
 
     num=1
 
-    for x in $(apg -n $passquantity -x $passlength -m $passlength);
+    for x in $(apg -n $passquantity -x $passlength -m $passlength -a $generationalgorithm);
 
     do echo -en "${ybold}$num${reset} - " 
 
@@ -52,4 +60,7 @@ else
 fi
 
 echo -e "${dashband}" >> $outputfilename
+
+
+
 
