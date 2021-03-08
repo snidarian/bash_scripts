d3331f2f581480328b9b4b92f36ff7c87525bc6b
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-03-08 09:51:00 -0500

    fixed bug
    Script had error when $set_minute was less than $current_minute - now its fixed

--- set_alarm.sh
+++ set_alarm.sh
@@ -3,6 +3,10 @@
 # path the final media player command to an alarm audio file
 # $1 sets alarm hour value, $2 sets alarm minute value (24 hour time keep in mind)
 
+# numberical constants
+minutes_in_hour=60
+hours_in_day=24
+
 
 set_hour=$1
 set_minute=$2
@@ -15,6 +19,7 @@ current_minute=`date "+%M"`
 
 echo "Current time: $current_hour:$current_minute"
 
+
 #LT
 if [ $current_hour -lt $set_hour ]; then
     hours_left=`expr $set_hour - $current_hour`
@@ -23,7 +28,9 @@ fi
 if [ $current_minute -lt $set_minute ]; then
     minutes_left=`expr $set_minute - $current_minute`
 fi
+
 #EQ
+
 if [ $current_hour -eq $set_hour ]; then
     hours_left=0
 fi
@@ -31,6 +38,7 @@ fi
 if [ $current_minute -eq $set_minute ]; then
     minutes_left=0
 fi
+
 #GT
 if [ $current_hour -gt $set_hour ]; then
     hours_left=`expr 24 - $(expr $current_hour - $set_hour)`
@@ -38,6 +46,12 @@ fi
 
 if [ $current_minute -gt $set_minute ]; then
     minutes_left=`expr 60 - $(expr $current_minute - $set_minute)`
+fi
+
+# Special catch case if set minute less than current minute subtract 1 from hours_left
+
+if [ $set_minute -lt $current_minute ]; then
+    hours_left=$(expr $hours_left - 1)
 fi
 
 # display time until alarm
@@ -50,14 +64,20 @@ else
 fi
 
 
+echo -ne "#          (10%)\r"
+sleep 1
+
+
+
+
 # sleep x number of hours and minutes
 sleep $hours_left\h
 sleep $minutes_left\m
 
+
+
 # alarm absolute filepath
 vlc --play-and-exit ~/Music/.user_sounds/fire_alarm.mp3
-
-
 
 
 
