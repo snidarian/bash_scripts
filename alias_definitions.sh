a83e0264d0fe8416b3918ce596b0c834fea9a778
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-03-16 13:20:17 -0400

    Update alias_definitions.sh

--- alias_definitions.sh
+++ alias_definitions.sh
@@ -10,6 +10,17 @@ alias clas='clear && ls -a'  # clears and list with hidden file flag specifier
 alias cllas='clear && ls -la' # Same but with long and hidden file flags                
 alias numberedls='j=0; for i in `ls`; do j=$(expr $j + 1); echo "$j. $i"; done' # numbers ls output. Can be useful. Has bug: doesn't handle whitespaces well but that's more a fault of the bash for loop than anything. I'll figure out how to fix it
 
+
+# Active terminal refresh and list contents in real time                                                                                                                                                           
+alias tcls='while [ 1 ]; do cls; sleep 1; done'
+alias tclls='while [ 1 ]; do clls; sleep 1; done'
+alias tclas='while [ 1 ]; do clas; sleep 1; done'
+alias tcllas='while [ 1 ]; do cllas; sleep 1; done'
+
+# colorize diff output - requires colordiff program                                                                                                                                                                
+alias diff='colordiff'
+
+
 # history search aliases
 alias tailhist='history | tail -n $1'                                                         
 alias histgrep='history | grep -i $1'
@@ -20,6 +31,7 @@ alias authorize='sudo chmod +766'
 alias deauthorize='sudo chmod -677'                                                                                              
                                                                                               
 # emacs alias definitions                                                                                                                                           
+alias enw='emacs -nw'
 alias anthomacs='emacs -nw -bg color-20 -fg yellow' 
 
 
