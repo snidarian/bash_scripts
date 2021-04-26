42a9e5de2905ea9634088a455855a536fdde51cc
snidarian <cephalopod31956@gmail.com>
2021-04-26 19:49:35 -0400

    woot

---
+++ install_essential_software.sh
@@ -0,0 +1,71 @@
+# This script quickly sets up a debian based system for my personal use
+
+
+echo "Setting up system essentials for $(id -un)@$(hostname)"
+
+
+# make sure system is up-to-date
+sudo apt update && sudo apt upgrade
+
+
+# ----------------------------------------------------------------
+# software list
+
+# emacs
+sudo apt-get install emacs
+# ccrypt
+sudo apt-get install ccrypt
+# net-tools (for ifconfig)
+sudo apt-get install net-tools
+# tclsh
+sudo apt-get install tclsh
+# Common LISP
+sudo apt-get install clisp
+# JDK
+sudo apt-get install default-jdk
+# openssh server
+sudo apt-get install openssh-server
+# install macchanger program
+sudo apt-get install macchanger
+
+# -------------------------------------------------------------------------
+# GIT SECTION
+
+# configure git credentials as snidarian
+git config --global user.name "snidarian"
+git config --global user.email "cephalopod31956"
+
+# make git_workfolders directory in home/user/ directory
+mkdir ~/git_workfolders
+cd ~/git_workfolders # cd directory to ~/git_workfolders before cloning all git repos
+
+# git repository clone list
+
+# dotfiles - important configuration files
+git@github.com:snidarian/dotfiles.git
+# bash scripts - essential bash scripts for various tasks
+git@github.com:snidarian/bash_scripts.git
+# sandbox - for experimentation and command practice
+git@github.com:snidarian/sandbox.git
+# Language_fundamentals - where I keep the syntax files for various langauges
+git@github.com:snidarian/Language_Fundamentals.git
+# syntax and refreshers for linux networking commands and theory
+git@github.com:snidarian/linux_networking.git
+
+
+# ----------------------------------------------------------
+# Setup home bin folder and add ~/bin to the PATH variable
+
+# make home bin directory
+mkdir ~/bin
+
+echo -e "\n\n\n----------------------------------------------" >> ~/.bashrc
+echo -e "# USER ADDED Add home bin directory to PATH" >> ~/.bashrc
+echo "PATH=\"$PATH:~/bin\"" >> ~/.bashrc
+
+
+# add essential aliases
+echo -e "\n\n# source aliases from bash_scripts to local .bashrc file" >> ~/.bashrc
+
+echo "source ~/git_workfolers/bash_scripts/alias_definitions.sh" >> ~/.bashrc
+
