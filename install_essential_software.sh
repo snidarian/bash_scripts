fefc18056e9f927d51f923000cef9e5a55b34139
snidarian <cephalopod31956>
2021-04-27 01:09:53 +0100

    setup scripts

--- install_essential_software.sh
+++ install_essential_software.sh
@@ -28,44 +28,5 @@ sudo apt-get install openssh-server
 # install macchanger program
 sudo apt-get install macchanger
 
-# -------------------------------------------------------------------------
-# GIT SECTION
-
-# configure git credentials as snidarian
-git config --global user.name "snidarian"
-git config --global user.email "cephalopod31956"
-
-# make git_workfolders directory in home/user/ directory
-mkdir ~/git_workfolders
-cd ~/git_workfolders # cd directory to ~/git_workfolders before cloning all git repos
-
-# git repository clone list
-
-# dotfiles - important configuration files
-git@github.com:snidarian/dotfiles.git
-# bash scripts - essential bash scripts for various tasks
-git@github.com:snidarian/bash_scripts.git
-# sandbox - for experimentation and command practice
-git@github.com:snidarian/sandbox.git
-# Language_fundamentals - where I keep the syntax files for various langauges
-git@github.com:snidarian/Language_Fundamentals.git
-# syntax and refreshers for linux networking commands and theory
-git@github.com:snidarian/linux_networking.git
-
-
-# ----------------------------------------------------------
-# Setup home bin folder and add ~/bin to the PATH variable
-
-# make home bin directory
-mkdir ~/bin
-
-echo -e "\n\n\n----------------------------------------------" >> ~/.bashrc
-echo -e "# USER ADDED Add home bin directory to PATH" >> ~/.bashrc
-echo "PATH=\"$PATH:~/bin\"" >> ~/.bashrc
-
-
-# add essential aliases
-echo -e "\n\n# source aliases from bash_scripts to local .bashrc file" >> ~/.bashrc
-
-echo "source ~/git_workfolers/bash_scripts/alias_definitions.sh" >> ~/.bashrc
+# more software items to come when I remember the rest
 
