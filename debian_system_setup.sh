dca36782fcf29ae1aa1a27ddec1fdbd3afe97204
snidarian <cephalopod31956@gmail.com>
2021-05-12 16:40:59 -0400

    change hostname functionality added

--- debian_system_setup.sh
+++ debian_system_setup.sh
@@ -175,9 +175,32 @@ elif [[ $rc_file == "zsh" ]]; then
 fi
 
 
+# PROMPT - ASK Y/N TO CHANGE HOSTNAME
+dialog --yesno "Your hostname is: $(hostname). \nWould you like to change it?" 15 60
+
+# Exit variable used in control flow
+change="$?"
+
+if [[ $change == 0 ]]; then
+    clear
+    echo -n "Enter new hostname: "
+    read new_hostname
+    sudo nano /etc/hostname
+    echo "Add your hostname manually to /etc/hosts file..."
+    sleep 3
+    sudo nano /etc/hosts
+    echo "The hostname of your public RSA key needs to be changed..."
+    sleep 3
+    sudo nano ~/.ssh/id_rsa.pub
+    echo "Now changing hostname in hostnamectl daemon"
+    sudo hostnamectl set-hostname "$new_hostname"
+elif [[ $change == 1 ]]; then
+    echo "Hostname remains unchanged"
+fi
+
+
+
 # List all Firefox browser plugins to install from the command line
-
-
 # Associative array for listing all browser add-ons/plugins:
 declare -A aa
 aa[0]="Ublock Origin"
