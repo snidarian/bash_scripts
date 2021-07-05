7003f7a9e4aa238e17bed81c3ea2ec3f9f99e591
snidarian <cephalopod31956@gmail.com>
2021-07-04 20:45:56 -0400

    improved getopts case statement, added color sequences, and country connect option

--- jvs.sh
+++ jvs.sh
@@ -2,9 +2,18 @@
 
 
 
+# Ansi color codes
+green="\e[1;32m"
+red="\e[1;31m"
+blue="\e[1;34m"
+cyan="\e[1;36m"
+reset="\e[0m"
+
+
+# --------------------------------------------------
 # function definitions
 
-function connectp2p()
+function connect_p2p()
 {
     
     echo "----------------------------------------------"
@@ -18,8 +27,14 @@ function connectp2p()
     echo "--------------------"
 }
 
+# list alpha-2 country codes
+function ls_alpha2_country_codes()
+{
+    echo -e "${green}Argentina: ${red}AR\n${green}Belgium: ${red}BE\n${green}Brazil: ${red}BR\n${green}Colombia: ${red}CO\n${green}Croatia: ${red}HR\n${green}Denmark: ${red}DK\n${green}Estonia: ${red}EE\n${green}Finland: ${red}FI\n${green}France: ${red}FR\n${green}Iceland: ${red}IS\n${green}Netherlands: ${red}NL\n${green}Singapore: ${red}SG\n${green}Switzerland: ${red}CH\n${green}South Africa: ${red}ZA\n${green}United States: ${red}US\n"
+}
 
-function connectRandom()
+
+function connect_random()
 {
     echo "------------------------------------------------"
     echo "########  Connecting to random server  #########"
@@ -33,7 +48,7 @@ function connectRandom()
 }
 
 
-function protonvpnStatus()
+function protonvpn_status()
 {
     echo "----------------------------------------------"
     echo "########          VPN STATUS        ##########"
@@ -56,33 +71,31 @@ function disconnect()
 # EXECUTIONS
 
 
-# jvs s
-if [ $1 == "s" ] || [ $1 == "status" ]
-then
-    protonvpnStatus
-fi
-
-# jvs p
-
-if [ $1 == "p" ] || [ $1 == "p2p" ]
-then
-    connectp2p
-fi
-
-
-# jvs r
-if [ $1 == "r" ] || [ $1 == "random" ]
-then
-    connectRandom
-fi
-
-
-# jvs d
-if [ $1 == "d" ] || [ $1 == "disconnect" ]
-then
-    disconnect
-fi
-
+while getopts :sdlpc: OPT; do
+    case $OPT in
+	s|+s)
+	    protonvpn_status
+	    ;;
+	l|+l)
+	    ls_alpha2_country_codes
+	    ;;
+	d|+d)
+	    disconnect
+	    ;;
+	p|+p)
+	    connect_p2p
+	    ;;
+	c|+c)
+	    #"$OPTARG"
+	    echo "The country you selected is $OPTARG"
+	    ;;
+	*)
+	    echo "usage: ${0##*/} [+-sdpc ARG} [--] ARGS..."
+	    exit 2
+    esac
+done
+shift $(( OPTIND - 1 ))
+OPTIND=1
 
 
 
