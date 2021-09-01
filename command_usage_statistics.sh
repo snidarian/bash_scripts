209df9ca58c2daa7d4bc891e92bac734e03cbb92
snidarian <cephalopod31956@gmail.com>
2021-08-31 22:42:16 -0400

    script begun

---
+++ command_usage_statistics.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/zsh
+
+# little script that displays the current usage counts for common commands
+
+
+for command in 'ls' 'pwd' 'cd'
+	       echo -n "$command: "
+
+
+	       # unfinished. gotta go to bed
+
+
+
+
+
+
+
+
+
+
+
+
