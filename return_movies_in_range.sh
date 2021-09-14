e65b9acddb7e8bcd06f46b84bec9c169d1937b19
snidarian <cephalopod31956@gmail.com>
2021-09-14 15:22:50 -0400

    script to save time searching for ansi color escape codes

--- return_movies_in_range.sh
+++ return_movies_in_range.sh
@@ -9,7 +9,7 @@ RESET="\e[0m"
 
 echo -e "\n\nOutputing movies from the year ${YELLOW}$1${RESET} to the year ${GREEN}$2${RESET}:\n"
 
-for x in "$(seq $1 1 $2)";
+for x in "$(seq $1 1 $2)"
 do
     ls | grep --color=auto -i "$x"
 done
