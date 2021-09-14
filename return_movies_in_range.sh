fb401b2bf3319feb7efae55d3e7f819ea417611d
snidarian <cephalopod31956@gmail.com>
2021-09-14 14:47:48 -0400

    added colorized output to command

--- return_movies_in_range.sh
+++ return_movies_in_range.sh
@@ -2,10 +2,16 @@
 # Return list of movies from range $1 to range $2
 
 
+GREEN="\e[1;32m"
+YELLOW="\e[1;33m"
+BLUE="\e[1;34m"
+RESET="\e[0m"
+
+echo -e "\n\nOutputing movies from the year ${YELLOW}$1${RESET} to the year ${GREEN}$2${RESET}:\n"
 
 for x in "$(seq $1 1 $2)";
 do
-    ls | grep -i "$x"
+    ls | grep --color=auto -i "$x"
 done
 	 
 
