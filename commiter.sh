f4c393a196d36acb26a680db7386faf81a478b60
snidarian <cephalopod31956@gmail.com>
2024-02-01 11:40:56 -0500

    internally generated commit ID#8182

--- commiter.sh
+++ commiter.sh
@@ -7,14 +7,13 @@ red="\e[1;31m"
 green="\e[1;32m"
 reset="\e[0m"
 
-# argv1 = number of commits to make
+# argv1 = номер коммитс
 
 commits_actual=0
 
 
 while [[ $commits_actual -lt $1 ]]; do
-    hexdump commiter.sh > file.txt
-#    echo -ne "commit|\n" >> file.txt
+    hexdump commiter.sh >> file.txt
     sleep .1
     git add -A
     sleep .1
