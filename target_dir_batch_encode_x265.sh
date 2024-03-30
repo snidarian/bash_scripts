cba12994d69328589eb92630ad4eb6259af04e99
snidarian <cephalopod31956@gmail.com>
2024-03-29 20:23:11 -0400

    fixed bugs

--- target_dir_batch_encode_x265.sh
+++ target_dir_batch_encode_x265.sh
@@ -24,7 +24,7 @@ reset="\e[0m"           #Reset
 
 
 
-while getopts :q:x:n:t:h OPT; do
+while getopts :q:x:n:t:r:h OPT; do
     case $OPT in
 	t|t+)
 	    target_dir="$OPTARG"
@@ -42,13 +42,19 @@ while getopts :q:x:n:t:h OPT; do
 	    quality="$OPTARG"
 	    echo "Quality = $quality"
 	    ;;
+	r|+r)
+	    # round cooldown (to allow coolant to cool betwwen multi-crf-round encodes)
+	    round_cooldown="$OPTARG"
+	    echo "Round cooldown set for $round_cooldown"
+	    ;;
 	h|+h)
 	    # Help message
 	    echo "HELP MESSAGE"
 	    echo -e " This shell script, using ffmpeg, encodes target directories to HEVC in .mkv container\nThe -q flag is a necessary parameter and must always be specified\nThe first arg to this script is the target dir to be converted\nby convention all encoded files will be sent to ~/Videos/staging/"
 	    ;;
 	*)
-	    echo "usage: `basename $0` [+-q ARG] [+-n ARG] [+-x ARG] [+-t ARG] [+-h} [--] ARGS..."
+	    echo "usage: `basename $0` [+-q ARG] [+-n ARG] [+-x ARG] [+-t ARG] [+-r ARG] [+-h} [--] ARGS..."
+	    echo -e "-q - crf quality (single-round)\n-n - min\n-x - max\n-t - target directory\n-r - cooldown time (multi-round)"
 	    exit 2
     esac
 done
@@ -84,15 +90,20 @@ cd "$target_dir"
 
 if [[ $multi_round == 1 ]] && [[ $single_round == 0 ]]; then
     echo "Initiating multi-round batch encode"
-    for crf_level in {$min..$max}; do
+    for crf_level in $(seq $min 1 $max); do
 	for file in *; do
 	    echo -e "${green}###########################################################${reset}"
-	    echo -e "Now encoding ${red}$file${reset} at CRF $quality"
+	    echo -e "Encoding ${red}$file${reset} at CRF $crf_level"
 	    echo -e "${green}###########################################################${reset}"
-	    ffmpeg -i "$file" -vcodec libx265 -crf $crf_level ~/Videos/staging/"$file"$quality.mkv;
+	    ffmpeg -i "$file" -vcodec libx265 -crf "$crf_level" ~/Videos/staging/crf/"$file"$crf_level.mkv;
 	done
 	# sleep for $round_cooldown duration (if set) allowing coolant liquid to left off heat between rounds
-	sleep $round_cooldown;
+	if [[ $round_cooldown != "" ]]; then
+	    echo -e "${red}Sleeping for $round_cooldown${reset}"
+	    sleep $round_cooldown;
+	else
+	    :
+	fi
     done
     
 elif [[ $multi_round == 0 ]] && [[ $single_round == 1 ]]; then
